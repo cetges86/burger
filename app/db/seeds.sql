6ad84af1b47fc7789faf07349645002acef264a2
Christopher Etges <c.etges86@gmail.com>
2018-05-09 21:55:28 -0600

    file setup

---
+++ app/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Egg Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mini Slider', false);
