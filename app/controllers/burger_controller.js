9743aa28ef00eb46c24cfe22ae32e5122b96c451
Christopher Etges <c.etges86@gmail.com>
2018-05-13 13:26:49 -0600

    logic working

--- app/controllers/burger_controller.js
+++ app/controllers/burger_controller.js
@@ -16,6 +16,12 @@ router.get("/", function (req, res) {
   });
 });
 
+router.get("/api/burgers", function(req,res){
+  burger.selectAll(function(data){
+    res.json(data);
+  })
+})
+
 router.get("/app/public/assets/css/burger_style.css", function (req, res) {
   res.sendFile(process.cwd() + "/app/public/assets/css/" + "burger_style.css");
 });
@@ -29,7 +35,7 @@ router.post("/api/burgers", function (req, res) {
   burger.insertOne([
     "burger_name", "devoured"
   ], [
-      req.body.burger_name, req.body.devoured
+      req.body.name, false
     ], function (result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
@@ -40,9 +46,10 @@ router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
+  console.log(req.body)
 
   burger.updateOne({
-    devoured: req.body.devoured
+    devoured: true
   }, condition, function (result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -52,19 +59,5 @@ router.put("/api/burgers/:id", function (req, res) {
     }
   });
 });
-
-// router.delete("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   burger.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -18,12 +18,17 @@ app.use(bodyParser.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main",
-layoutsDir: path.join(__dirname, "app/views/layouts") }));
+// app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, "/app/views/layouts/"),
+  partialsDir: path.join(__dirname, "/app/views/partials/")
+}));
 
 app.set("view engine", "handlebars");
 app.set('views', path.join(__dirname, "/app/views"));
-console.log(path.join(__dirname, "/app/views"));
+// app.set('partials', path.join(__dirname, "/app/views/partials"))
 
 // Import routes and give the server access to them.
 var routes = require("./app/controllers/burger_controller.js");
@@ -31,7 +36,7 @@ var routes = require("./app/controllers/burger_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
